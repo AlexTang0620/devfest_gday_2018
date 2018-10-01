54880d047c76eef9cda9bdafd970c3d410461b8d
AlexTang0620 <alext970620@gmail.com>
2018-10-02 00:08:33 +0100

    Relocate the code for google sign in pop up

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -40,6 +40,16 @@ export class NavbarComponent implements OnInit {
      * google log in provider
      */
     public googleSignIn() {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+        }).catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+      });
         // this.doGoogleSignIn()
         //     .then(res => {
         //         // create avatar
