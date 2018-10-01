6a428b701c03580c2e8c06e6a92b1bb2f1eb1f1c
AlexTang0620 <alext970620@gmail.com>
2018-10-02 00:24:57 +0100

    Remove code from index.html

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -42,7 +42,7 @@ export class NavbarComponent implements OnInit {
     public googleSignIn() {
         var provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then(function (result) {
-          var token = result.credential.accessToken;
+        //   var token = result.credential.accessToken;
           var user = result.user;
         }).catch(function (error) {
           var errorCode = error.code;
