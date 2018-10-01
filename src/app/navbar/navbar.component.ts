9c44b7dbdae227b8caf4675547f59d2d43652d32
Jason Seah <jason@greenroom.com.my>
2018-10-01 22:57:28 +0800

    Update angular set style default to scss and also add bootstrap 4

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,7 +5,7 @@ import * as firebase from 'firebase/app';
 @Component({
     selector    : 'app-navbar',
     templateUrl : './navbar.component.html',
-    styleUrls   : ['./navbar.component.css']
+    styleUrls   : ['./navbar.component.scss']
 })
 
 export class NavbarComponent implements OnInit {
