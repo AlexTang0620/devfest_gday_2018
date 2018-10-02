5eb3c64ed300123840d7586c2399146cb96296c5
AlexTang0620 <alext970620@gmail.com>
2018-10-03 00:09:09 +0100

    Changed firebase hosting destination, Removed default firebase public folder, added firebase element to environment production"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AngularFireModule} from 'angularfire2';
 import {AngularFirestoreModule} from 'angularfire2/firestore';
 import {AngularFireAuthModule} from 'angularfire2/auth';
-import {environment} from '../environments/environment';
+import {environment} from '../environments/environment.prod';
 
 const appRoutes: Routes = [
     {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase   : {
+    apiKey            :"AIzaSyBJ2PmqzSuGhM0RcBaf336DLbZz5dFC0g4",
+    authDomain        :"devfest-gday.firebaseapp.com",
+    databaseURL       :"https://devfest-gday.firebaseio.com",
+    projectId         :"devfest-gday",
+    storageBucket     :"devfest-gday.appspot.com",
+    messagingSenderId :"369571999405"
+}
 };
