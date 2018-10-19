ce03afe5a7362e1e762b43170336eadbee7a66c5
louisyoong <louis@greenroom.com.my>
2018-10-19 18:40:20 +0800

    Add favicon icon.

--- src/app/models/Schedule.ts
+++ src/app/models/Schedule.ts
@@ -1,7 +1,4 @@
-import {Course} from "./Course";
-import {Observable} from "rxjs";
-
 export interface Schedule {
     id: string;
-    courses: Observable<Course[]>;
+    courses: any;
 }
--- src/app/services/schedule.service.ts
+++ src/app/services/schedule.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
-import {Observable} from "rxjs";
 import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
-import {Schedule} from "../models/Schedule";
-import {map} from "rxjs/operators";
 import * as firebase from 'firebase';
 import DocumentReference = firebase.firestore.DocumentReference;
-import {Course} from "../models/Course";
-import {CourseService} from "./course.service";
+import {Schedule} from '../models/Schedule';
+import {Observable} from 'rxjs';
+import {CourseService} from './course.service';
+import {map} from 'rxjs/operators';
+
 
 @Injectable({
     providedIn : 'root'
@@ -27,36 +27,15 @@ export class ScheduleService {
             .snapshotChanges()
             .pipe(
                 map(schedules => schedules.map(schedule => {
-                    let scheduleData: Schedule = schedule.payload.doc.data();
+                    const scheduleData: Schedule = schedule.payload.doc.data();
                     const id = schedule.payload.doc.id;
 
                     scheduleData.courses = scheduleData.courses
-                        .map(course => this.courseService.getCourse(course.path))
+                        .map(course => this.courseService.getCourse(course.path));
 
                     return {id, ...scheduleData};
                 }))
             );
-    }
-
-    //     this.scheduleService.get().subscribe(schedule => {
-    //     console.log(schedule)
-    //     // this.schedule = schedule;
-    // });
-
-    async getCourseDetail(courses) {
-        return await courses.courses.map(course => {
-            course.get()
-                .then(res => {
-                    console.log(res.data())
-                    // return res.data()
-                });
-        })
-    }
-
-    async getSpeakerDetail(speaker: DocumentReference) {
-        return await speaker.get()
-            .then(speaker => {
-            });
     }
 }
 
